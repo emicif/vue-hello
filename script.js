876506d56d17780e046f8239e7e69a6842004536
emicif <emanuela.cifolelli@gmail.com>
2022-03-24 13:37:01 +0100

    img

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ console.log('JS VUE OK!');
 
 
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data
-
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data. 
 
 const app = new Vue (
     {
     el: '#root',
     data: {
-        titolo: 'Hello World!',
+        titolo: 'Boolean #57',
+        sottotitolo: 'Emanuela Cifolelli',
         paragrafo: 'La prima prova con VUE JS',
-        mieClassi: 'text-property',
+        textProperty: 'text-property',
+        textPropertySottotitolo: 'text-propriety-sottotitolo',
+        image: 'https://michelenasti.com/images/vuejs.png'
     }
     }
 );
+
+
